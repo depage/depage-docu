91407992ef7c875db982f2b107b0becfee805d09
Frank Hellenkamp <jonas@depage.net>
2018-09-19 18:18:28 +0200

    enhanced navigation with mobile menu

--- www/lib/global/js/global.js
+++ www/lib/global/js/global.js
@@ -31,161 +31,6 @@ function getHexColorFromString(colorString) {
 }
 /* }}} */
 
-// javascript flash detection
-// {{{ jquery.browser.iphone
-jQuery.browser.iphone = function() {
-    return (/iphone/).test(navigator.userAgent.toLowerCase());
-}();
-// }}}
-// {{{ jquery.browser.has3d
-jQuery.extend(jQuery.browser, {
-    has3d: (function () {
-        var el = document.createElement('p'), 
-            has3d,
-            transforms = {
-                'webkitTransform':'-webkit-transform',
-                'OTransform':'-o-transform',
-                'msTransform':'-ms-transform',
-                'MozTransform':'-moz-transform',
-                'transform':'transform'
-            };
-
-        // Add it to the body to get the computed style.
-        document.body.insertBefore(el, null);
-
-        for (var t in transforms) {
-            if (el.style[t] !== undefined) {
-                el.style[t] = "translate3d(1px,1px,1px)";
-                has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
-            }
-        }
-
-        document.body.removeChild(el);
-
-        return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
-    })
-});
-// }}}
-// {{{ jquery.flash
-jQuery.fn.flash = function(params) {
-    var html1 = "";
-    var html2 = "";
-    var flashParam = [];
-
-    for (var p in params.params) {
-        flashParam.push(p + "=" + encodeURI(params.params[p]));
-    }
-
-    //object part
-    html1 += "<object type=\"application/x-shockwave-flash\" ";
-    html1 += "data=\"" + params.src + "?" + flashParam.join("&amp;") + "\" ";
-    if (params.width !== undefined) {
-        html1 += "width=\"" + params.width + "\" ";
-    }
-    if (params.height !== undefined) {
-        html1 += "height=\"" + params.height + "\" ";
-    }
-    if (params.className !== undefined) {
-        html1 += "class=\"" + params.className + "\" ";
-    }
-    if (params.id !== undefined) {
-        html1 += "id=\"" + params.id + "\" ";
-    }
-
-    //param part
-    html2 += "<param name=\"movie\" value=\"" + params.src + "?" + flashParam.join("&amp;") + "\" />";
-
-    if (params.transparent === true) {
-        html1 += "mwmode=\"transparent\"";
-        html2 += "<param name=\"wmode\" value=\"transparent\" />";
-    }
-    html1 += ">";
-
-    return $(html1 + html2 + "</object>");
-};
-// }}}
-
-// embedded jquery plugins
-// {{{ jquery pause
-/*
- * Jonathan Howard
- *
- * jQuery Pause
- * version 0.2
- *
- * Requires: jQuery 1.0 (tested with svn as of 7/20/2006)
- *
- * Feel free to do whatever you'd like with this, just please give credit where
- * credit is do.
- *
- *
- *
- * pause() will hold everything in the queue for a given number of milliseconds,
- * or 1000 milliseconds if none is given.
- *
- *
- *
- * unpause() will clear the queue of everything of a given type, or 'fx' if no
- * type is given.
- */
-
-$.fn.pause = function(milli,type) {
-        milli = milli || 1000;
-        type = type || "fx";
-        return this.queue(type,function(){
-                var self = this;
-                setTimeout(function(){
-                        $.dequeue(self);
-                },milli);
-        });
-};
-
-$.fn.clearQueue = $.fn.unpause = function(type) {
-        return this.each(function(){
-                type = type || "fx";
-                if(this.queue && this.queue[type]) {
-                        this.queue[type].length = 0;
-                }
-        });
-};
-// }}}
-// {{{ jquery fx custom
-jQuery.fx.prototype.custom = function(from, to, unit){
-    this.startTime = (new Date()).getTime();
-    this.start = from;
-    this.end = to;
-    this.unit = unit || this.unit || "px";
-    this.now = this.start;
-    this.pos = this.state = 0;
-    this.update();
-
-    var self = this;
-    function t(){
-        return self.step();
-    }
-
-    t.elem = this.elem;
-
-    jQuery.timers.push(t);
-
-    if ( jQuery.timers.length == 1 ) {
-        var timer = setInterval(function(){
-            var timers = jQuery.timers;
-            
-            for ( var i = 0; i < timers.length; i++ ) {
-                if ( !timers[i]() ) {
-                    timers.splice(i--, 1);
-                }
-            }
-
-            if ( !timers.length ) {
-                clearInterval( timer );
-            }
-        }, 75);
-    }
-};
-// }}}
-
 // replace content, depending on reader capabilities
 // {{{ replaceInteractiveContent()
 function replaceInteractiveContent() {
@@ -246,13 +91,13 @@ function replaceInteractiveContent() {
             .text(text_source_showplain)
             .next().show().next().hide();
     });
-    
+
     // style source code
     //prettyPrint();
     // }}}
-     // {{{ change height of iframe
-    $("iframe[seamless]").iframeAutoHeight({
-        heightOffset: 30
+    // {{{ add burger menu
+    $("#main-nav").on("click", function() {
+        $(this).toggleClass("active");
     });
     // }}}
 }
@@ -289,19 +134,7 @@ $(document).ready(function() {
 
     $(window).on("statechangecomplete", replaceInteractiveContent);
     $(window).on("scroll resize", onDocumentScroll);
-
-    // add 3d flag
-    if ($.browser.has3d()) {
-        $("html").addClass("css-3dtransform");
-    }
-
-    // hide urlbar
-    if ($.browser.iphone) {
-        $(window).load(function() {
-            this.scrollTo(0, 1);
-        });
-    }
 });
 // }}}
-    
+
 /* vim:set ft=javascript sw=4 sts=4 fdm=marker : */
